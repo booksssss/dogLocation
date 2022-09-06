2c9555f695717c007b0bb69872e38988ebe38e07
booksssss <86739545+booksssss@users.noreply.github.com>
2022-09-05 19:42:36 -0700

    Add files via upload

---
+++ Screens/AddDogDEtails.js
@@ -0,0 +1,251 @@
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+  Image,
+  ScrollView,
+  TextInput,
+  Dimensions,
+  Button,
+  Alert
+} from "react-native";
+import DropDownPicker from "react-native-dropdown-picker";
+import { RFValue } from "react-native-responsive-fontsize";
+import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
+import firebase from "firebase";
+
+let customFonts = {
+  "Bubblegum-Sans": require("../assets/fonts/BubblegumSans-Regular.ttf")
+};
+
+export default class AddDogDEtails extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fontsLoaded: false,
+   
+    };
+  }
+
+  async _loadFontsAsync() {
+    await Font.loadAsync(customFonts);
+    this.setState({ fontsLoaded: true });
+  }
+
+  componentDidMount() {
+    this._loadFontsAsync();
+    this.fetchUser();
+  }
+
+  async addDogDetails() {
+  
+  }
+
+  fetchUser = () => {
+    let theme;
+    firebase
+      .database()
+      .ref("/users/" + firebase.auth().currentUser.uid)
+      .on("value", snapshot => {
+        theme = snapshot.val().current_theme;
+        this.setState({ light_theme: theme === "light" });
+      });
+  };
+
+  render() {
+    if (!this.state.fontsLoaded) {
+      return <AppLoading />;
+    } else {
+      return null 
+      };
+      return (
+        <View
+          style={
+            this.state.light_theme ? styles.containerLight : styles.container
+          }
+        >
+          <SafeAreaView style={styles.droidSafeArea} />
+          <View style={styles.appTitle}>
+            <View style={styles.appIcon}>
+              <Image
+                source={require("../assets/dogIcon.png")}
+                style={styles.iconImage}
+              ></Image>
+            </View>
+            <View style={styles.appTitleTextContainer}>
+              <Text
+                style={
+                  this.state.light_theme
+                    ? styles.appTitleTextLight
+                    : styles.appTitleText
+                }
+              >
+                New Details
+              </Text>
+            </View>
+          </View>
+          <View style={styles.fieldsContainer}>
+            <ScrollView>
+              <Image
+                source={preview_images[this.state.previewImage]}
+                style={styles.previewImage}
+              ></Image>
+
+              <View style={{ height: RFValue(this.state.dropdownHeight) }}>
+               
+              </View>
+              <View style={{ marginHorizontal: RFValue(10) }}>
+                <TextInput
+                  
+                  onChangeText={title => this.setState({ title })}
+                  placeholder={"Title"}
+                 
+                />
+                <TextInput
+                  style={[
+                    
+                    styles.inputFontExtra,
+                    styles.inputTextBig
+                  ]}
+                 
+                  placeholder={"Description"}
+                  multiline={true}
+                  numberOfLines={4}
+                 
+                />
+                <TextInput
+                  style={[
+                   
+                    styles.inputFontExtra,
+                    styles.inputTextBig
+                  ]}
+                 
+                  placeholder={"Story"}
+                  multiline={true}
+                  numberOfLines={20}
+                 
+                />
+                <TextInput
+                  style={[
+                    
+                    styles.inputFontExtra,
+                    styles.inputTextBig
+                  ]}
+                 
+                  placeholder={"Moral of the story"}
+                  multiline={true}
+                  numberOfLines={4}
+                  
+                />
+              </View>
+              <View style={styles.submitButton}>
+                <Button
+                  onPress={() => this.addDogDetails()}
+                  title="Submit"
+                  color="#841584"
+                />
+              </View>
+            </ScrollView>
+          </View>
+          <View style={{ flex: 0.08 }} />
+        </View>
+      );
+    }
+  }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#15193c"
+  },
+  containerLight: {
+    flex: 1,
+    backgroundColor: "white"
+  },
+  droidSafeArea: {
+    marginTop: Platform.OS === "android" ? StatusBar.currentHeight : RFValue(35)
+  },
+  appTitle: {
+    flex: 0.07,
+    flexDirection: "row"
+  },
+  appIcon: {
+    flex: 0.3,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  iconImage: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "contain"
+  },
+  appTitleTextContainer: {
+    flex: 0.7,
+    justifyContent: "center"
+  },
+  appTitleText: {
+    color: "white",
+    fontSize: RFValue(28),
+    fontFamily: "Bubblegum-Sans"
+  },
+  appTitleTextLight: {
+    color: "black",
+    fontSize: RFValue(28),
+    fontFamily: "Bubblegum-Sans"
+  },
+  fieldsContainer: {
+    flex: 0.85
+  },
+  previewImage: {
+    width: "93%",
+    height: RFValue(250),
+    alignSelf: "center",
+    borderRadius: RFValue(10),
+    marginVertical: RFValue(10),
+    resizeMode: "contain"
+  },
+  inputFont: {
+    height: RFValue(40),
+    borderColor: "white",
+    borderWidth: RFValue(1),
+    borderRadius: RFValue(10),
+    paddingLeft: RFValue(10),
+    color: "white",
+    fontFamily: "Bubblegum-Sans"
+  },
+  inputFontLight: {
+    height: RFValue(40),
+    borderColor: "black",
+    borderWidth: RFValue(1),
+    borderRadius: RFValue(10),
+    paddingLeft: RFValue(10),
+    color: "black",
+    fontFamily: "Bubblegum-Sans"
+  },
+  dropdownLabel: {
+    color: "white",
+    fontFamily: "Bubblegum-Sans"
+  },
+  dropdownLabelLight: {
+    color: "black",
+    fontFamily: "Bubblegum-Sans"
+  },
+  inputFontExtra: {
+    marginTop: RFValue(15)
+  },
+  inputTextBig: {
+    textAlignVertical: "top",
+    padding: RFValue(5)
+  },
+  submitButton: {
+    marginTop: RFValue(20),
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
---
+++ Screens/Dashboard.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+  Image,
+  Dimensions
+} from "react-native";
+
+
+
+export default class Dashboard extends Component {
+
+    render() {
+        return(
+            <View>
+                <Text>HEllo </Text>
+            </View>
+        )
+    }
+}
